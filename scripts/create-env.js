ae41abc56071779beecd230d8b5c7fdca68d9222
Victor Areiza <victorareiza02@gmail.com>
2023-02-05 16:31:53 -0500

    porfin o que

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,16 +1,2 @@
 const fs = require(`fs`);
-
-try {
-  if (!process.env.API) {
-    throw new Error('La variable de entorno "API" no está definida.');
-  }
-
-  const data = `API=${process.env.API}\n`;
-  if (fs.existsSync('./.env')) {
-    console.warn('.env ya existe, se sobreescribirá el archivo.');
-  }
-  fs.writeFileSync('./.env', data);
-  console.log('Se ha escrito en .env correctamente.');
-} catch (error) {
-  console.error(error);
-}
+fs.writeFileSync('./.env', `API=${process.env.API}\n`);
