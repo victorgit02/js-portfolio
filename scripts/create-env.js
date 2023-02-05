932531a36f4855d5758242b85a73b35c7f8dc0cb
Victor Areiza <victorareiza02@gmail.com>
2023-02-05 16:22:38 -0500

    changes for last time

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,2 +1,16 @@
 const fs = require(`fs`);
-fs.writeFileSync('./.env', `API=${process.env.API}\n`);
+
+try {
+  if (!process.env.API) {
+    throw new Error('La variable de entorno "API" no está definida.');
+  }
+
+  const data = `API=${process.env.API}\n`;
+  if (fs.existsSync('./.env')) {
+    console.warn('.env ya existe, se sobreescribirá el archivo.');
+  }
+  fs.writeFileSync('./.env', data);
+  console.log('Se ha escrito en .env correctamente.');
+} catch (error) {
+  console.error(error);
+}
